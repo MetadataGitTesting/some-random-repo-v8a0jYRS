3d2f07061922ee7b30bcd617cd9471f9b5fa2694
sudhanshuyadav2@gmail.com <sudhanshuyadav2@gmail.com>
2022-01-05 11:43:42 +0000

    test_another

--- code/scala/workflows/Test-Workflow/code/src/main/scala/main.scala
+++ code/scala/workflows/Test-Workflow/code/src/main/scala/main.scala
@@ -11,7 +11,8 @@ import graph._
 object Main {
 
   def apply(spark: SparkSession): Unit = {
-    val df_Source_0 = Source_0(spark)
+    val df_Source_0   = Source_0(spark)
+    val df_Reformat_1 = Reformat_1(spark, df_Source_0)
   }
 
   def main(args: Array[String]): Unit = {
